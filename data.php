705e10e2faf3bf2f9b6c77ffd6bb9299d4e682e4
sukanyachrt <sukanya.chrt@gmail.com>
2024-01-18 22:31:42 +0700

    add page settingpayment.html เพื่อใช้ในการตั้งค่าค่าใช้จ่าย

--- data.php
+++ data.php
@@ -5,3 +5,6 @@ $data = isset($_GET['v']) ? $_GET['v'] : '';
 if ($data == "receiveData") {
     echo json_encode([$_POST,$_FILES]);
 }
+else if($data=="settingPayment"){
+    echo json_encode($_POST);
+}
